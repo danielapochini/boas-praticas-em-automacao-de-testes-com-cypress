1969d872a992fc9d5b348146ff478f096d397ca3
danielapochini <daniela.pochini@bs2tecnologia.com.br>
2022-03-25 08:31:29 -0300

    Exercício Flaky Test

--- cypress/integration/flaky-test/sample.spec.js
+++ cypress/integration/flaky-test/sample.spec.js
@@ -1,11 +1,14 @@
 describe('Flaky tests bad practice', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories'}
+    ).as('getStories')
+    
     cy.visit('https://wlsf82-hacker-stories.web.app')
 
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
+    //cy.wait('@getStories')
   })
 
   Cypress._.times(10, () => {
@@ -16,11 +19,8 @@ describe('Flaky tests bad practice', () => {
         cy.search(faker.random.word())
       })
 
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
-
+      //cy.wait('@getStories')
+     
       cy.get('.last-searches button')
         .should('have.length', 5)
     })
